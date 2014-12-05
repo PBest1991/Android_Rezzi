ec60c554ad920e6d99a9121fbd75b45363f067e7
seancmurphy <sean.con.murphy@gmail.com>
2014-12-05 11:31:06 -0500

    what

---
+++ app/src/main/java/testing/gromobi/com/androidfunctions/HomeActivity.java
@@ -0,0 +1,63 @@
+package testing.gromobi.com.androidfunctions;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+
+public class HomeActivity extends Activity {
+
+    //instantiation of elements
+    TextView txtvTitle;
+    Button btnNavPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+    //action called from pressing btnNavPhone
+    public void openCallActivity(View view) {
+        Intent openCallIntent = new Intent(this, PhoneCallActivity.class);
+        startActivity(openCallIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();  // Always call the superclass
+
+        // Stop method tracing that the activity started during onCreate()
+        android.os.Debug.stopMethodTracing();
+    }
+}
